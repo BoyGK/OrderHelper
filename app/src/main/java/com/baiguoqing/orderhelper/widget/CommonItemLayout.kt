daf0bfb5a89d7bf591af451b02cd34b007db9cfe
BaiGuoQing <1215356195@qq.com>
2019-12-14 17:58:09 +0800

    bgq

---
+++ app/src/main/java/com/baiguoqing/orderhelper/widget/CommonItemLayout.kt
@@ -0,0 +1,9 @@
+package com.baiguoqing.orderhelper.widget
+
+import android.content.Context
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class CommonItemLayout(context: Context?) : ConstraintLayout(context) {
+
+    constructor(context: Context?,)
+}
